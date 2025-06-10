33035d8271e5c771fd687d7ec3dac1bc3b376da5
Lance <SoccerLanceTobey@gmail.com>
2025-06-09 23:55:50 -0600

    Update GUIControl.py
    Added API functionality to the GUI, shifted around items and page size to give a better feel.

--- GUIControl.py
+++ GUIControl.py
@@ -15,20 +15,26 @@ API_KEY = os.getenv('OPENWEATHER_API_KEY')
 gui = Tk()
 
 gui.title("Weather Around The World!")
-gui.geometry('360x720')
+gui.geometry('360x240')
 
 introLabel = Label(gui, text="Enter a city and country name to find your location.")
-introLabel.grid()
+introLabel.grid(column = 1, row = 0)
+
+cityLabel = Label(gui, text="City:")
+cityLabel.grid(column = 0, row = 1)
+
+stateLabel = Label(gui, text="State:")
+stateLabel.grid(column = 0, row = 2)
 
 citySelector = Entry(gui)
-citySelector.grid()
+citySelector.grid(column=1, row=1)
 
 stateSelector = ttk.Combobox(gui, values = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Conneticut", "Delaware", "Florida", "Georgia",
                                                                              "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
                                                                              "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
                                                                              "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
                                                                              "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"])
-stateSelector.grid()
+stateSelector.grid(column=1, row=2)
 
 
 def findData():
@@ -44,21 +50,34 @@ def findData():
     params = {
         "q": f"{citySelector.get()},{stateSelector.get()},{"US"}",
         "appid": API_KEY,
-        "units": "imperial"  # or "metric"
+        "units": "imperial"
     }
     
     response = requests.get(base_url, params=params)
     data = response.json()
     
     if response.status_code == 200:
-        temp = data["main"]["temp"]
-        desc = data["weather"][0]["description"]
-        print(f"{citySelector.get()}, {stateSelector.get()}: {temp}°F, {desc}")
+        temperature = data["main"]["temp"]
+        weather = data["weather"][0]["description"]
+        vibes = data["main"]["feels_like"]
+        speed = data["wind"]["speed"]
+        humid = data["main"]["humidity"]
+
+
+        temperatureLabel = Label(gui, text = f"The current temperature is: {temperature}°F.") 
+        temperatureLabel.grid(column = 1, row = 4)
+        feelsLikeLabel = Label(gui, text = f"The current  feels like: {vibes}°F.") 
+        feelsLikeLabel.grid(column = 1, row = 5)
+        cloudCover = Label(gui, text = f"The current weather is: {weather}.") 
+        cloudCover.grid(column = 1, row = 6)
+        windSpeed = Label(gui, text = f"The current wind speed is: {speed}.") 
+        windSpeed.grid(column = 1, row = 7)
+        humidity = Label(gui, text = f"The humidity percentage is {humid}%.") 
+        humidity.grid(column = 1, row = 8)
     else:
         print(f"Error {response.status_code}: {data.get('message', 'Unknown error')}")
 
-
 enterSelection = Button(gui, text= "Press here to confirm location", command=findData)
-enterSelection.grid()
+enterSelection.grid(column = 1, row = 3)
 
 gui.mainloop()
